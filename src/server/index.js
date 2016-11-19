077d8231a9d334b7b7fef53b7e5b51ca0e8f0d3b
rchasman <rchasman@gmail.com>
2016-11-18 16:17:09 -0800

    RDC - Slight cleanup.

--- src/server/index.js
+++ src/server/index.js
@@ -2,21 +2,25 @@
 
 import config from './config';
 
+let trendsData = [];
+
 function getTrends(trends) {
   fetch('http://crossorigin.me/http://hawttrends.appspot.com/api/terms/')
     .then(resp => resp.json())
     .then((json) => {
-      let trendsData = [];
-      json[1].slice(0, 5).map(name => getTrendImage(trends, trendsData, name));
+      json[1].slice(0, 5).map(name => getTrendImage(trends, name));
     })
     .catch((err) => {
       console.log('Fetching 5 Trends Failed', err);
     });
 }
 
-function getTrendImage(trends, trendsData, trend) {
-  fetch(`https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${trend}&count=3&offset=0&mkt=en-us`,
-    {headers: {'Ocp-Apim-Subscription-Key': config.bingImageSearch.key}})
+function getTrendImage(trends, trend) {
+  fetch(`https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${trend}&count=3&offset=0&mkt=en-us`, {
+    headers: {
+      'Ocp-Apim-Subscription-Key': config.bingImageSearch.key,
+    },
+  })
   .then(resp => resp.json())
   .then((json) => {
     let randImgPicker = Math.floor(Math.random() * 3);
