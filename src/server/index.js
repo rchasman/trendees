1f3862bf9cd222a2bd0db6340b15b3a77fa7d6e0
rchasman <rchasman@gmail.com>
2016-11-21 09:51:26 -0800

    RDC - Much more clean way to get trends via promises.

--- src/server/index.js
+++ src/server/index.js
@@ -7,26 +7,29 @@ let trendsData = [];
 function getTrends(trends) {
   fetch('http://crossorigin.me/http://hawttrends.appspot.com/api/terms/')
     .then(resp => resp.json())
-    .then((json) => {
-      json[1].slice(0, 5).map(name => getTrendImage(trends, name));
+    .then(json => {
+      const promises = json[1].slice(1, 5).map(getTrendImage);
+      return Promise.all(promises)
     })
-    .catch((err) => {
+    .then(trendData => {
+      trends.set(trendData)
+    })
+    .catch(err => {
       console.log('Fetching 5 Trends Failed', err);
     });
 }
 
-function getTrendImage(trends, trend) {
-  fetch(`https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${trend}&count=3&offset=0&mkt=en-us`, {
+function getTrendImage(trend) {
+  return fetch(`https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${trend}&count=3&offset=0&mkt=en-us`, {
     headers: {
       'Ocp-Apim-Subscription-Key': config.bingImageSearch.key,
     },
   })
   .then(resp => resp.json())
-  .then((json) => {
+  .then(json => {
     let randImgPicker = Math.floor(Math.random() * 3);
     let imageSrc = json.value[randImgPicker].contentUrl;
-    trendsData.push({ name: trend, imageSrc });
-    trends.set(trendsData);
+    return { name: trend, imageSrc };
   })
   .catch((err) => {
     console.log('Fetching Trend Image Failed', err)
